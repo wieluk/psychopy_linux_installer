8aea4026b871a2720fbeef12349ea7443e245c10
Lukas Wiertz <wieluk@mailbox.org>
2024-07-18 11:40:07 +0200

    action check

--- .github/workflows/psychopy_test.py
+++ .github/workflows/psychopy_test.py
@@ -1,8 +1,27 @@
-import psychopy
+from psychopy import core, data, sound
 
-# Create core.Clock instance and test it
+# Create core.Clock instance
 clock = core.Clock()
 
+# Define trial conditions
+trial_conditions = [{'trialNum': 1, 'waitTime': 1.0}, {'trialNum': 2, 'waitTime': 2.0}, {'trialNum': 3, 'waitTime': 3.0}]
+
+# Create a TrialHandler to manage these conditions
+trial_data = data.TrialHandler(trialList=trial_conditions, nReps=1, method='random')
+
+# Run through each trial
+for trial in trial_data:
+    # Reset the clock at the beginning of each trial
+    clock.reset()
+
+    # Wait for the specified time for this trial
+    core.wait(trial['waitTime'])
+
+    # Print the trial number and the elapsed time
+    print(f"Trial {trial['trialNum']}: Elapsed time {clock.getTime()} seconds")
+
+    # Wait for the sound to finish
+    core.wait(0.5)
 
 # Exit PsychoPy to clean up everything properly
 core.quit()
