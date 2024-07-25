2ce4a9d45da1aea31923541b83f4b5f69417e7e9
Lukas Wiertz <wieluk@mailbox.org>
2024-07-25 15:16:07 +0200

    wording

--- psychopy_linux_installer.sh
+++ psychopy_linux_installer.sh
@@ -336,7 +336,7 @@ PYTHON_VERSION_CLEAN=$(echo "${PYTHON_VERSION}" | tr -d ',;')
 
 # Check PSYCHOPY_VERSION
 if [ -n "$PSYCHOPY_VERSION_CLEAN" ] && version_greater_than "$PSYCHOPY_VERSION_CLEAN" "2023.2.3" && { [ "$OS_VERSION" = "debian-11" ] || [ "$OS_VERSION" = "ubuntu-18.04" ]; }; then
-    read -p "Your PsychoPy version ($PSYCHOPY_VERSION_CLEAN) is higher than 2023.2.3 and might require manual fixes. Do you want to change it to the stable version 2023.2.3? (y/N): " change_version
+    read -p "Your PsychoPy version ($PSYCHOPY_VERSION_CLEAN) is higher than 2023.2.3 and might require manual fixes on $OS_VERSION. Do you want to change it to the stable version 2023.2.3? (y/N): " change_version
     if [ "$change_version" = "y" ] || [ "$change_version" = "Y" ]; then
         PSYCHOPY_VERSION="2023.2.3"
         echo "PsychoPy version changed to 2023.2.3."
@@ -344,7 +344,6 @@ if [ -n "$PSYCHOPY_VERSION_CLEAN" ] && version_greater_than "$PSYCHOPY_VERSION_C
         echo "Keeping PsychoPy version $PSYCHOPY_VERSION."
     fi
 fi
-
 
 # Create PsychoPy directory
 PSYCHOPY_DIR="${INSTALL_DIR}/psychopy_${PSYCHOPY_VERSION_CLEAN}_py_${PYTHON_VERSION_CLEAN}"
